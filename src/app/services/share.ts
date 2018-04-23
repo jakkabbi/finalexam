cca5736229982780e402e23f7932c85ed88515c2
jakkabbi <Mcclure.jack.e@gmail.com>
2018-04-23 15:37:12 -0400

    fixed CORS
    connects properly to EC2 now

--- src/app/services/share.ts
+++ src/app/services/share.ts
@@ -10,31 +10,31 @@ export class ShareService {
   constructor(private http: HttpClient) { }
 
   getShares(user: String): Observable<Share[]> {
-    return this.http.get<Share[]>(`http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/shares/${user}`);
+    return this.http.get<Share[]>(`/api/shares/${user}`);
   }
 
   countShares(): Observable<number> {
-    return this.http.get<number>('http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/shares/count');
+    return this.http.get<number>('/api/shares/count');
   }
 
   addShare(share: Share): Observable<Share> {
-    return this.http.post<Share>('http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/share', share);
+    return this.http.post<Share>('/api/share', share);
   }
 
   getShare(share: Share): Observable<Share> {
-    return this.http.get<Share>(`http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/share/${share._id}`);
+    return this.http.get<Share>(`/api/share/${share._id}`);
   }
 
   getPrice(share: Share): Observable<number> {
-    return this.http.get<number>(`http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/price/${share.tickerName}`);
+    return this.http.get<number>(`/api/price/${share.tickerName}`);
   }
 
   editShare(share: Share): Observable<string> {
-    return this.http.put(`http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/share/${share._id}`, share, { responseType: 'text' });
+    return this.http.put(`/api/share/${share._id}`, share, { responseType: 'text' });
   }
 
   deleteShare(share: Share): Observable<string> {
-    return this.http.delete(`http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/share/${share._id}`, { responseType: 'text' });
+    return this.http.delete(`/api/share/${share._id}`, { responseType: 'text' });
   }
 
 }
--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -10,35 +10,35 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   register(user: User): Observable<User> {
-    return this.http.post<User>('http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/user', user);
+    return this.http.post<User>('/api/user', user);
   }
 
   login(credentials): Observable<any> {
-    return this.http.post<any>('http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/login', credentials);
+    return this.http.post<any>('/api/login', credentials);
   }
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>('http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/users');
+    return this.http.get<User[]>('/api/users');
   }
 
   countUsers(): Observable<number> {
-    return this.http.get<number>('http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/users/count');
+    return this.http.get<number>('/api/users/count');
   }
 
   addUser(user: User): Observable<User> {
-    return this.http.post<User>('http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/user', user);
+    return this.http.post<User>('/api/user', user);
   }
 
   getUser(user: User): Observable<User> {
-    return this.http.get<User>(`http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/user/${user._id}`);
+    return this.http.get<User>(`/api/user/${user._id}`);
   }
 
   editUser(user: User): Observable<string> {
-    return this.http.put(`http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/user/${user._id}`, user, { responseType: 'text' });
+    return this.http.put(`/api/user/${user._id}`, user, { responseType: 'text' });
   }
 
   deleteUser(user: User): Observable<string> {
-    return this.http.delete(`http://ec2-52-15-39-153.us-east-2.compute.amazonaws.com:3000/api/user/${user._id}`, { responseType: 'text' });
+    return this.http.delete(`/api/user/${user._id}`, { responseType: 'text' });
   }
 
 }
